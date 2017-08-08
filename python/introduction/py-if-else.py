036a80e0625a548320153ef326d75c8e0c7c8657
Hugo Martins <caramelo.martins@gmail.com>
2017-08-08 22:37:15 +0100

    Add Python Introduction exercises

---
+++ python/introduction/py-if-else.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#
+# https://www.hackerrank.com/challenges/py-if-else
+
+if __name__ == '__main__':
+    n = int(input())
+
+    if n % 2 != 0:
+        print("Weird")
+    elif n % 2 == 0 and n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n % 2 == 0 and n >= 6 and n <= 20:
+        print("Weird")
+    elif n % 2 == 0 and n > 20:
+        print("Not Weird")
---
+++ python/introduction/python-loops.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#
+# https://www.hackerrank.com/challenges/python-loops
+
+if __name__ == '__main__':
+    n = int(input())
+
+    for i in range(n):
+        print(i * i)
